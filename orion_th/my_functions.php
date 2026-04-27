cca1fd9d38dd7792624615543d95512d437f54a9
Daniil <m2140sr@gmail.com>
2026-04-27 17:24:55 +0300

    add mu-plagins v3

--- orion_th/my_functions.php
+++ orion_th/my_functions.php
@@ -7,6 +7,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.2' );
+}
+
 add_theme_support('post-thumbnails');
 
 register_nav_menus();
@@ -17,11 +22,11 @@ function orion_th_scripts_style()
 	wp_enqueue_script('swiper', get_template_directory_uri() . '/assets/js/swiper-bundle.min.js', array('jquery'), null, true);
 	wp_enqueue_script('fancybox', get_template_directory_uri() . '/assets/js/jquery.fancybox.min.js', array('jquery'), null, true);
 	wp_enqueue_script('mixitup', get_template_directory_uri() . '/assets/js/mixitup.min.js', array('jquery'), null, true);
-	wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true);
+	wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), _S_VERSION, true);
 	
 	wp_enqueue_style('swiper-bundle', get_template_directory_uri() . '/assets/css/swiper-bundle.min.css', array(), null, 'all');
 	wp_enqueue_style('fancybox', get_template_directory_uri() . '/assets/css/jquery.fancybox.min.css', array(), null, 'all');
-	wp_enqueue_style('orion_th-style', get_stylesheet_uri(), array(), null);
+	wp_enqueue_style('orion_th-style', get_stylesheet_uri(), array(), _S_VERSION);
 }
 
 
